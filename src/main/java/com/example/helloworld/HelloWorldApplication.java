a5f7fb1db76fa65b7c23d8eb6246be881b017127
nicholaslippis <nick@grepr.io>
2024-11-13 13:38:53 -0800

    hello world application

---
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,26 @@
+package com.example.helloworld;
+
+import com.example.helloworld.resources.HelloWorldResource;
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        environment.jersey().register(new HelloWorldResource());
+    }
+}
---
+++ src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,6 @@
+package com.example.helloworld;
+
+import io.dropwizard.core.Configuration;
+
+public class HelloWorldConfiguration extends Configuration {
+}
